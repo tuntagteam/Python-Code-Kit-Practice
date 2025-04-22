e0b6424cd982629fcd5d9f861c1fa597b943363a
Tagwithsaltedcaramel <koontackch@gmail.com>
2025-04-22 16:40:12 +0700

    change to for loop

--- Practice 2B/3b-06.py
+++ Practice 2B/3b-06.py
@@ -1,11 +1,13 @@
 user_input = input("กรอกคะแนน (คั่นด้วยช่องว่าง): ")
 scores = list(map(int, user_input.strip().split()))
 
-total = sum(scores)
-count = len(scores)
+total = 0
+count = 0
+
+for score in scores:
+    total += score
+    count += 1
+
 average = total / count
 
 print("{:.2f}".format(average))
-
-
-
