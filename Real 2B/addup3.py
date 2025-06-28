b504b919b756d947611cc57e3a8c57f63960ec90
tagwithsaltedcaramel <chawadol.suwansanit@truecodingschool.com>
2025-06-28 17:07:35 +0700

    Add initial implementation of Magic Number Filter application with styling

---
+++ Real 2B/addup3.py
@@ -0,0 +1,7 @@
+x = int(input("Enter x"))
+y = int(input("Enter y"))
+z = int(input("Enter z"))
+
+sum = x + y + z
+
+print("The sum of", x, ",", y, "and", z, "is", sum)
---
+++ web-project/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET' , 'POST'])
+def index():
+    numbers = random.sample(range(1,51),20)
+    magic_numbers = []
+
+    if request.method == 'POST':
+        magic_numbers = list(filter(lambda x:x % 2 == 0 or x % 3 == 0 or x**2 < 100, numbers))
+
+    return render_template('index.html' ,numbers=numbers, magic_numbers=magic_numbers)
+
+if __name__ == '__main__':
+    app.run(debug=True)
