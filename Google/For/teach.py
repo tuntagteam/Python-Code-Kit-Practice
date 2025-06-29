dd20e1a6f967edd894633c809e4411fe5c0fee32
tagwithsaltedcaramel <chawadol.suwansanit@truecodingschool.com>
2025-06-29 12:55:49 +0700

    Add initial implementation of teaching scripts in teach.py and writealong.py

---
+++ Google/For/teach.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    print("I love python")
+
+for i in range(5, 0, -1):
+    print("Countdown:", i)
+print("Liftoff!")
+
+for i in range(1, 11):
+    print("3 x", i, "=", 3*i)
+
+num = int(input("Enter number: "))
+for i in range(1, 11):
+    print(num, "x", i, "=", num*i)
+
+for hour in range(1, 9):
+    print("Hour", hour, ": Don't forget to drink water!")
+
+for step in range(0, 21, 2):
+    print("Step", step)
+
+for i in range(10, 0, -2):
+    print(i)
