aa5a16be4d52a2da0acbd4f85a8434145770df06
tagwithsaltedcaramel <koontackch@gmail.com>
2026-05-07 17:15:31 +0700

    Create main.py

---
+++ eric/main.py
@@ -0,0 +1,23 @@
+# Data Types
+# String = a-z 0-9 @%^&!$%@^&*$%!^@#&
+# Integer = 0-9
+# Float = 0.00 - n.mm
+# Boolean = true false 0 , 1
+# List = ["a" , "b" ,"c"]
+# Dict = {}
+
+
+# math equalation
+# + add
+# - minus
+# * multiply
+# / divide
+# // divide no remainder
+# % only remainder
+
+a = 1
+b = 3
+a = 34
+
+print("Hello",a,"Hello")
+print(a)
