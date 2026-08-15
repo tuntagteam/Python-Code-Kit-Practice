24584bb3034fd4cecae737f6c91be4dc1e6a2bd3
tagwithsaltedcaramel <koontackch@gmail.com>
2026-08-15 12:27:18 +0700

    Some

---
+++ python_Friday/ques.py
@@ -0,0 +1,33 @@
+# name = input("กรอกชื่อ :")
+# age = int(input("กรอกอายุ :"))
+
+# print("สวัสดี",name)
+# print("คุณอายุ",age)
+
+# score = int(input("Enter your score :"))
+# if score >= 50:
+#     print("Passed")
+# else:
+#     print("Failed")
+
+# if age > 12:
+#     print("Can study python")
+# else:
+#     print("Start with Scratch")
+
+# for i in range(5):
+#     print("Hello",name)
+
+# for j in range(1,11):
+#     print(j)
+
+# for k in range(1,11):
+#     if k >= 5:
+#         print(k,"เลขมากหรือเท่ากับ 5")
+#     else:
+#         print(k,"เลขน้อยกว่า 5")
+
+a = int(input("Enter Number :"))
+for i in range(a):
+    if i % 2 == 0:
+        print(i) 
