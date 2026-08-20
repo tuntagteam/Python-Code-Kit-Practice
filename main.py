d7591c2bd1eab0126dfbbe977cb4e43912e47fea
tagtruecoding <chawadol.suwansanit@truecodingschool.com>
2026-08-20 13:56:40 +0700

    stuff

---
+++ main.py
@@ -0,0 +1,4 @@
+a = 10
+b = 8
+print(a+b)
+print(a-b)
---
+++ python_sunday/christian/forloop.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your number :"))
+for i in range(1,13):
+    print(f"{a} x {i} = {a*i}")
+
+# start 0
+# stop 13
+# step 1
+
+# for i in range(2,8,2):
+#     print(i)
