91a6854922ba2c86a99f8d3b27a15efd186d6031
tagtruecoding <chawadol.suwansanit@truecodingschool.com>
2026-08-28 16:52:07 +0700

    ros lesson stuff

---
+++ python_Friday/ros/main.py
@@ -0,0 +1,47 @@
+# import random
+
+# x = random.randint(1,10)
+# y = int(input("Enter your number :"))
+
+# while x != y:
+#     print("Incorrect")
+#     y = int(input("Enter your number :"))
+    
+# print("Correct")
+
+# password = "recon123"
+# x = input("Enter your password")
+# while password != x:
+#     print("Incorrect")
+#     x = input("Enter your password")
+# print("Hello World")
+
+# for i in range(1,11):
+#     print(i)
+
+# x = 1
+# while x != 11:
+#     print(x)
+#     x+=1
+
+# password = "python123"
+# x = input("Enter password : ")
+
+# while x != password:
+#     print("Incorrect")
+#     x = input("Enter password : ")
+
+# print("Correct")
+
+i = 10
+while i != 0:
+    print(i)
+    i-=1
+print("Go!")   
+
+x = 7
+y = int(input("Enter number :"))
+while x != y:
+    print("Incorrect!")
+    y = int(input("Enter number :"))
+print("Correct")
