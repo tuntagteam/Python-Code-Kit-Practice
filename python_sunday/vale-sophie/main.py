8c3706a994cbd2ac7f8571a86b0c9b980ee70a7a
tagtruecoding <chawadol.suwansanit@truecodingschool.com>
2026-08-30 15:44:10 +0700

    Create main.py

---
+++ python_sunday/vale-sophie/main.py
@@ -0,0 +1,7 @@
+# name = "Jonathan Joestar"
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+
+print("My name is ",name)
+print(f"I'm {age} years old")
+print("In 5 years. I will be",age+5,"years old!")
