d6db0a8c7ed317ce3ed47fbf7356c6b3670476f1
tagtruecoding <chawadol.suwansanit@truecodingschool.com>
2026-09-20 18:47:35 +0700

    chirstian teaching

---
+++ christian/homework3.py
@@ -0,0 +1,9 @@
+# Create todolists using Def While Lists to store the data
+# Functionality
+# 1. Add new to do list
+# 2. Edit the existing todo list
+# 3. List/Show all to do list
+# 4. Delete the existing todo list
+# 5. Quit the program
+# 6. No AI
+# 7. Google is allow
--- christian/jjj.py
+++ christian/jjj.py
@@ -1,11 +1,11 @@
-Enter a number: 100
+food_brand = ["McDonalds","KFC","Burkey Queen","Dairy King","Pizza Castle"]
+food_food = ["SmallMac","Biscuits","Whoppy","Blizzarrrd","Pepperoni"]
 
-1 is Odd
-2 is Even
-3 is Odd
-4 is Even
-5 is Odd
-6 is Even
-...
-100 is Even
+print(*food_brand)
+print(*food_food)
 
+for food_brand,food_food in zip(food_brand,food_food):
+    print(f"I went to {food_brand} to order {food_food}")
+
+for i in food_food:
+    print(food_food[i])
