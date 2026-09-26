f9d5a7cd295d64252d1da26955083478211a3d6c
tagtruecoding <chawadol.suwansanit@truecodingschool.com>
2026-09-26 12:33:45 +0700

    Gimhor

---
+++ Gimhor/input123.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number :")) # 5 , 6 
+b = int(input("Enter second number :")) # 5 , 7
+
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} × {b} = {a*b}")
+print(f"{a} ÷ {b} = {a/b}")
+
+# {a} + {b} = {a+b}
+# 5 + 5 = 10
+
+# + Plus / Add
+# - Minus / Subtract
+# * Multiply / Time
+# / Divide
